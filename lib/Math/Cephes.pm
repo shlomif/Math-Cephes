0ca209dd84bde3e0f91cc213eb66023b86c3e619
Randy Kobes <randy.kobes@gmail.com>
2009-01-05 17:39:32 -0800

    import Math-Cephes 0.47 from CPAN
    git-cpan-module:   Math-Cephes
    git-cpan-version:  0.47
    git-cpan-authorid: RKOBES
    git-cpan-file:     authors/id/R/RK/RKOBES/Math-Cephes-0.47.tar.gz

--- lib/Math/Cephes.pm
+++ lib/Math/Cephes.pm
@@ -57,7 +57,7 @@ my @fract = qw(radd rsub rmul rdiv euclid);
 	      @explog, @bessels, @gammas, @betas, @elliptics, 
 	      @hypergeometrics, @misc, @dists, @fract, @cmplx);
 		
-$VERSION = '0.46';
+$VERSION = '0.47';
 #Math::Cephes->bootstrap($VERSION);
 #var_Math__Cephes_init();
 
--- lib/Math/Cephes/Polynomial.pm
+++ lib/Math/Cephes/Polynomial.pm
@@ -11,7 +11,7 @@ $fflag = 0;
 require Exporter;
 *import = \&Exporter::import;
 @EXPORT_OK = qw(poly);
-$VERSION = '0.46';
+$VERSION = '0.47';
 
 require Math::Cephes;
 require Math::Cephes::Fraction;
